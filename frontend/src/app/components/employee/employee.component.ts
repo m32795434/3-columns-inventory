f14321453772013e7229c0dea50ca0734fec3a30
Manuel Augusto Bravard <manubravard@gmail.com>
2023-07-14 12:00:33 -0300

    some updates and better code

--- frontend/src/app/components/employee/employee.component.ts
+++ frontend/src/app/components/employee/employee.component.ts
@@ -20,30 +20,35 @@ export class EmployeeComponent implements OnInit {
 
   getEmployees() {
     this.employeeService.getEmployees().subscribe(
-      (res) => {
-        //we store the data in "EmployeeService.employees". It's an array with ALL the employees. I must do it better.
-        this.employeeService.employees = res as [];
-      },
-      (err) => console.error(err)
+      {
+        next:(res) => {
+          this.employeeService.employees = res as [];//I think a better approach would be using a Subject or a setter. Not this way.💡 
+        },
+        error:(err) => console.error(err)
+      }
     );
   }
   addEmployee(form: NgForm) {
     // to determinate, if it's an editing or creating method
     if (this.employeeService.selectedEmployee._id) {
       this.employeeService.updateEmployee(form.value).subscribe(
-        (res) => console.log(res),
-        (err) => console.error(err)
+        {
+          next:(res) => console.log(res),
+          error:(err) => console.error(err)
+        }
       );
     } else {
       if (confirm('Are you sure you want to create employee?')) {
         this.employeeService.createEmployee(form.value).subscribe(
-          // it creates the employee, but doesn't call getEmployees(),or resetForm. What's the problem?
-          (res) => {
-            this.getEmployees();
-            form.reset();
-            console.log(res);
-          },
-          (err) => console.error(err)
+          {
+            // it creates the employee, but doesn't call getEmployees(),or resetForm. What's the problem?
+            next:(res) => {
+              this.getEmployees();
+              form.reset();
+              console.log(res);
+            },
+            error:(err) => console.error(err)
+          }
         );
       }
     }
@@ -52,11 +57,13 @@ export class EmployeeComponent implements OnInit {
   deleteEmployee(_id: string) {
     if (confirm('Are you sure you want to delete this employee?')) {
       this.employeeService.deleteEmployee(_id).subscribe(
-        (res) => {
-          this.getEmployees();
-          console.log(res);
-        },
-        (err) => console.error(err)
+        {
+          next:(res) => {
+            this.getEmployees();
+            console.log(res);
+          },
+          error:(err) => console.error(err)
+        }
       );
     }
   }
